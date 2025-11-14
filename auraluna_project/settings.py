08971ba7dd8a8b4dc1e8620a12c6c07677617c07
orhungkdmr <orhungokdemir@gmail.com>
2025-11-14 03:44:21 +0300

    Remove Whitenoise from installed apps
    Remove Whitenoise from the development server settings.

--- auraluna_project/settings.py
+++ auraluna_project/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'cloudinary_storage', 
     'django.contrib.staticfiles', 
     'cloudinary', 
-    'whitenoise.runserver_nostatic', # Geliştirme sunucusunda static'i devre dışı bırakır
+
 
     # Kendi Uygulamalarımız
     'products',
